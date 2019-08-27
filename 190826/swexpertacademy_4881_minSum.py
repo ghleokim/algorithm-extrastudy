6cbb7ef25a9bf2f85877ae232ed9f31cf100391a
Leo Kim <gh.leokim@gmail.com>
2019-08-27 14:41:48 +0900

    27Aug2019

--- 190826/swexpertacademy_4881_minSum.py
+++ 190826/swexpertacademy_4881_minSum.py
@@ -2,6 +2,7 @@
 """
 N * N 배열에서 N개의 숫자를 골라 합이 최소가 되도록.
 세로로 같은 줄에서 두 개 이상의 숫자를 고르지 않도록 한다.
+가지치기 중요.
 """
 
 def perm(k, s=0):
@@ -9,19 +10,18 @@ def perm(k, s=0):
     if k == N:
         if s < res:
             res = s
-        print()
-        print(res)
     else:
         if s > res:
-            print()
-            print(s)
+            # 탈출조건: 현재까지의 sum > 현재까지의 minimum result
             return 0
         for i in range(N):
             if not visited[i]:
                 print(i, end=' ')
+                # 다음순열
                 visited[i] = 1
                 s += board[k][i]
                 perm(k+1, s)
+                # 순열 빠져나옴
                 s -= board[k][i]
                 visited[i] = 0
 
@@ -40,7 +40,10 @@ for T in range(int(input())):
 
     print('#{0} {1}'.format(T+1, res))
 
+
+
 """
+TC
 3
 3
 2 1 2
